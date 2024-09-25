c131e351873b6169acf252e52649786519d857fb
Abhishek <abhishekgoel@gofynd.com>
2024-09-25 09:24:29 +0530

    server created using http module

--- BE/POC/3_http_module.js
+++ BE/POC/3_http_module.js
@@ -1,17 +1,40 @@
 // http modules allows u to create a basic http server 
 //servers handles incoming requests and sedn responses . 
 
-
-const { write } = require("fs");
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const server = http.createServer((req,res)=>{
     //this cb is going to be called for each incoming http request
+    console.log("Request URL", req.url);
+    console.log("Request Method", req.method);
     
-    res.setHeader("Content-Type", 'text/plain');
-
-    res.write("Hello, World !!");
-
-    res.end();
+    if(req.url == "/about"){
+        res.setHeader("Content-Type", 'text/plain');
+        res.write("Hello, this is About Page !!");
+    }else if(req.url == "/home"){
+        res.setHeader("Content-Type", 'text/html');
+        const filePath =  path.resolve("index.html");
+        console.log(filePath);
+        fs.readFile(filePath, 'utf-8', (err,data)=>{
+            if(err){
+                console.log(err);
+                res.write(`Internal server error , ${err.message}`)
+            }else{
+                console.log(data);
+                res.end(data);
+            }
+        })
+    }else if(req.url == "/favicon.ico"){
+        res.setHeader("Content-Type", 'image/x-icon');
+        res.end();
+        return;
+    }else{
+        res.setHeader("Content-Type", 'text/plain');
+        // res.write("This url does nto exist , 404 ");
+        // res.end();
+        res.end("This url does nto exist , 404 ")
+    }
 
 })
 
